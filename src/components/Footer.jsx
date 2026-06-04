d551056e2d7fc58e6636e0876af936a03667686c
Beatrice-Beacco <beatrice.beacco@gmail.com>
2026-06-04 14:57:19 +0200

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  margin-top: 3rem;
+  padding-top: 1rem;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  text-align: center;
+  color: #08060d;
+  background-color: white;
+`;
+
+const FooterText = styled.p`
+  margin: 0;
+`;
 
 const Footer = () => {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <FooterContainer>
+      <FooterText>© {new Date().getFullYear()} AI&CS Projects</FooterText>
+    </FooterContainer>
   );
 };
 
