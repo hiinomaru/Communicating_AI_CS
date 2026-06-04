c35ac479a3f18a36702dc2bbf0fb1df0e2230d64
Beatrice-Beacco <beatrice.beacco@gmail.com>
2026-06-04 15:42:24 +0200

    feat: project component and grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ProjectCard from "./components/ProjectCard";
 import { projects } from "./data/projects";
+import styled from "styled-components";
+
+const ProjectsGrid = styled.div`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+`;
 
 export default function App() {
   return (
@@ -15,24 +31,17 @@ export default function App() {
       <Header />
       <h1>AI&CS Projects</h1>
 
-      <div style={{ display: "grid", gap: 20 }}>
+      <ProjectsGrid>
         {projects.map((p, i) => (
-          <a
+          <ProjectCard
             key={i}
-            href={p.url}
-            target="_blank"
-            style={{
-              padding: 20,
-              background: "#1e293b",
-              borderRadius: 12,
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            {p.name}
-          </a>
+            name={p.name}
+            url={p.url}
+            description={p.description}
+            image={p.image}
+          />
         ))}
-      </div>
+      </ProjectsGrid>
       <Footer />
     </div>
   );
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,50 @@
 export const projects = [
   {
     name: "Fracture Detection",
-    url: "https://hiinomaru.github.io/"
+    url: "https://hiinomaru.github.io/",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-X7tW-_kQR-4eXya7J1MXb5txqMn53wHQ0w&s",
+    description:
+      "A project that detects fractures in medical images using deep learning.",
+  },
+  {
+    name: "Fracture Detection",
+    url: "https://hiinomaru.github.io/",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-X7tW-_kQR-4eXya7J1MXb5txqMn53wHQ0w&s",
+    description:
+      "A project that detects fractures in medical images using deep learning.",
+  },
+  {
+    name: "Fracture Detection",
+    url: "https://hiinomaru.github.io/",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-X7tW-_kQR-4eXya7J1MXb5txqMn53wHQ0w&s",
+    description:
+      "A project that detects fractures in medical images using deep learning.",
+  },
+  {
+    name: "Fracture Detection",
+    url: "https://hiinomaru.github.io/",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-X7tW-_kQR-4eXya7J1MXb5txqMn53wHQ0w&s",
+    description:
+      "A project that detects fractures in medical images using deep learning.",
+  },
+  {
+    name: "Fracture Detection",
+    url: "https://hiinomaru.github.io/",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-X7tW-_kQR-4eXya7J1MXb5txqMn53wHQ0w&s",
+    description:
+      "A project that detects fractures in medical images using deep learning.",
+  },
+  {
+    name: "Fracture Detection",
+    url: "https://hiinomaru.github.io/",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-X7tW-_kQR-4eXya7J1MXb5txqMn53wHQ0w&s",
+    description:
+      "A project that detects fractures in medical images using deep learning.",
   },
 ];
